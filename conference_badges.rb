a22cfc86de5a03d5941ffe404611345c14bb90cd
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-09-21 21:43:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,6 +17,9 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  batch_badge_creator(attendees).each do |result|
+    puts result
+  assign_rooms(attendees).each do |result|
+    puts result
+  end
 end
